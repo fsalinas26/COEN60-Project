72409803e8f311564f124168fa9315e2664eaa6f
Truong, David <dtruong@scu.edu>
2022-11-29 21:48:52 -0800

    Update

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
-    <div class="header">
+        <div class="header">
             <a href="index.php"><img id="logo" src="./images/scu_logo.png"></a>
             <h1 id="title_text">Senior Design Projects</h1>
         </div>
@@ -25,43 +25,55 @@
             </div> 
             <a href="professors.php">PROFESSORS</a>
         </div>
-        <?php
-            $json = file_get_contents("./data.json");
-            $json_data = json_decode($json,true);
+        <div id="main">
+            <div id="filter">
+                <p>test</p>
+            </div>
+            <div id="info">
+                <?php
+                    $json = file_get_contents("./data.json");
+                    $json_data = json_decode($json,true);
 
-            foreach ($json_data as $item) {
-                echo "<h3>".$item["title"]." - ";
+                    foreach ($json_data as $item) {
+                        echo '<div class="info-item">';
+                        echo "<h3>".$item["title"]." - ";
 
-                if (gettype($item["major"]) == "array") {
-                    for ($i = 0; $i < count($item["major"]); $i++) {
-                        echo $item["major"][$i];
-                        if ($i != count($item["major"]) - 1) {
-                            echo ", ";
+                        if (gettype($item["major"]) == "array") {
+                            for ($i = 0; $i < count($item["major"]); $i++) {
+                                echo $item["major"][$i];
+                                if ($i != count($item["major"]) - 1) {
+                                    echo ", ";
+                                }
+                            }
                         }
-                    }
-                }
-                else {
-                    echo $item["major"];
-                }
-                echo " - ".$item["year"]."</h3>";
-                echo "<p><b>Advisors: </b>";
-                if (gettype($item["advisor"]) == "array") {
-                    echo "<h4>";
-                    for ($i = 0; $i < count($item["advisor"]); $i++) {
-                        echo $item["advisor"][$i];
-                        if ($i != count($item["advisor"]) - 1) {
-                            echo ", ";
+                        else {
+                            echo $item["major"];
                         }
+                        echo " - ".$item["year"]."</h3>";
+                        echo "<p><b>Advisors: </b>";
+                        if (gettype($item["advisor"]) == "array") {
+                            echo "<h4>";
+                            for ($i = 0; $i < count($item["advisor"]); $i++) {
+                                echo $item["advisor"][$i];
+                                if ($i != count($item["advisor"]) - 1) {
+                                    echo ", ";
+                                }
+                            }
+                            echo "</h4>";
+                        }
+                        else {
+                            echo $item["advisor"]."</p>";
+                        }
+                        if (strlen($item["abstract"]) > 100)
+                            echo "<p><b>Abstract: </b>".substr($item["abstract"], 0, 500)."...</p>";
+                        else {
+                            echo "<p><b>Abstract: </b>".$item["abstract"]."</p>";
+                        }
+                        echo "<a href=".$item["url"].">Link to Project PDF</a><br><br>";
+                        echo "</div>";
                     }
-                    echo "</h4>";
-                }
-                else {
-                    echo $item["advisor"]."</p>";
-                }
-                if ()
-                echo "<p><b>Abstract: </b>".$item["abstract"]."</p>";
-                echo "<a href=".$item["url"].">Link to Project PDF</a><br><br>";
-            }
-        ?>
+                ?>
+            </div>
+        </div>
     </body>
 </html>
