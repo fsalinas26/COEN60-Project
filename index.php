3d763ba801799ddad1b9a0f0da1e75a4f46dc59e
Francisco Salinas <salinasf3@gmail.com>
2022-11-29 19:55:55 -0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <div class="header">
+            <img id="logo" src="./images/scu_logo.png">
+            <h1 id="title_text">Senior Design Projects</h1>
+        </div>
+        <div class="navbar">
+            <a href="about.php">ABOUT</a>
+            <div class="dropdown">
+                <button class="dropbtn">YEARS
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="#">2022</a>
+                    <a href="#">2021</a>
+                    <a href="#">2020</a>
+                    <a href="#">More...</a>
+                </div>
+            </div> 
+            <div class="dropdown">
+                <button class="dropbtn">MAJORS
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="#">Bioengineering</a>
+                    <a href="#">Civil Engineering</a>
+                    <a href="#">Computer Engineering</a>
+                    <a href="#">More...</a>
+                </div>
+            </div> 
+            <div class="dropdown">
+                <button class="dropbtn">PROFESSORS 
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                    <a href="#">More...</a>
+                </div>
+            </div> 
+        </div>
+    </body>
+</html>
