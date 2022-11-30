2007c4b2d9affb55d7dd2880f05c34be869527e8
Francisco Salinas <salinasf3@gmail.com>
2022-11-29 20:56:59 -0800

    Added reactive boxes

--- index.php
+++ index.php
@@ -46,5 +46,13 @@
                 </div>
             </div> 
         </div>
+        <div class="container">
+            <div class="box">
+            </div>
+            <div class="box">
+            </div>
+            <div class="box">
+            </div>
+        </div>
     </body>
 </html>
